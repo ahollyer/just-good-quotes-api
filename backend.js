555a27b94a76ee5150d77f6e72cebfb864b29a27
Aspen Hollyer <aspen.dollins@gmail.com>
2017-07-13 12:50:18 -0500

    Send json data

--- backend.js
+++ backend.js
@@ -1,13 +1,12 @@
 'use strict';
 const express = require('express');
 const app = express();
-// const body_parser = require('body-parser');
-
-/************ Database *************/
+const body_parser = require('body-parser');
 const promise = require('bluebird');
-const pgp = require('pg-promise')({
-  promiseLib: promise
-});
+const pgp = require('pg-promise')({ promiseLib: promise });
+
+
+/************************ Database Configuration ***************************/
 const db = pgp({
   host: 'localhost',
   port: 9002,
@@ -15,30 +14,38 @@ const db = pgp({
   user: 'postgres',
 });
 
-// NOTE: Sample query: Un-Comment to check connection to database
-db.query("SELECT * FROM quote")
-  .then(function(results) {
-    results.forEach(function(row) {
-        console.log(row.text);
-    });
-  //   return db.one("SELECT * FROM restaurant WHERE name='tout suit'");
- })
+  //////////////////////////////////////////////////////////////
+ // Sample query: Un-comment to check connection to database //
+//////////////////////////////////////////////////////////////
+// db.query("SELECT * FROM quote")
+//   .then(function(results) {
+//     results.forEach(function(row) {
+//         console.log(row.text);
+//     });
+// });
 
 
-
-
+/************************** App Configuration *******************************/
 app.set('view engine', 'hbs');
-// app.use(body_parser.urlencoded({extended: false}));
+app.use(body_parser.urlencoded({extended: false}));
 app.use('/static', express.static('public'));
 
-app.use(function (request, response, next) {
-  console.log(request.method, request.path);
-  next();
+  ////////////////////////////////////////////////
+ // Un-comment to log incoming server requests //
+////////////////////////////////////////////////
+// app.use(function (request, response, next) {
+//   console.log(request.method, request.path);
+//   next();
+// });
+
+
+/********************************* Routes ***********************************/
+app.get('/', function (request, response) {
+  response.json(
+    {message: 'This is an API Chump!'}
+  );
 });
 
-
-
-/************ Routes ****************/
 app.listen(8000, function() {
   console.log('Listening on port 8000');
 });
