ed0625bcdaf48fd68b76891abc9bf52e6febb71d
Aspen Hollyer <aspen.dollins@gmail.com>
2017-07-13 12:17:10 -0500

    Connect backend to database'

--- backend.js
+++ backend.js
@@ -1,15 +1,34 @@
+'use strict';
 const express = require('express');
 const app = express();
-const body_parser = require('body-parser');
+// const body_parser = require('body-parser');
 
-var promise = require('bluebird');
-var pgp = require('pg-promise')({
+/************ Database *************/
+const promise = require('bluebird');
+const pgp = require('pg-promise')({
   promiseLib: promise
 });
-var db = pgp({database: 'restaurant'});
+const db = pgp({
+  host: 'localhost',
+  port: 9002,
+  database: 'Quotes',
+  user: 'postgres',
+});
+
+// NOTE: Sample query: Un-Comment to check connection to database
+db.query("SELECT * FROM quote")
+  .then(function(results) {
+    results.forEach(function(row) {
+        console.log(row.text);
+    });
+  //   return db.one("SELECT * FROM restaurant WHERE name='tout suit'");
+ })
+
+
+
 
 app.set('view engine', 'hbs');
-app.use(body_parser.urlencoded({extended: false}));
+// app.use(body_parser.urlencoded({extended: false}));
 app.use('/static', express.static('public'));
 
 app.use(function (request, response, next) {
@@ -19,7 +38,7 @@ app.use(function (request, response, next) {
 
 
 
-
+/************ Routes ****************/
 app.listen(8000, function() {
   console.log('Listening on port 8000');
 });
